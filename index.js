4bd36ab827d89b1cb6cb6da978d9e4fd0d2132bc
soriful15 <sorifulislam.rs10@gmailcomm>
2023-07-25 01:25:01 +0600

    express,mongodb,jwt token done

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// middleware
+require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.uwuwq9x.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    const userCollection = client.db('resturentBooking').collection('UserCollection')
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
